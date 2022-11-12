54da2e7292495a72948aeea113b01f727cf8fb1e
Do Tuan <tuanm.dev@gmail.com>
2022-11-12 18:41:33 +0700

    [leetcode] add problem longest-substring-without-repeating-characters

---
+++ src/main/java/dev/tuanm/sandbox/leetcode/problem/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,44 @@
+package dev.tuanm.sandbox.leetcode.problem;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dev.tuanm.sandbox.leetcode.annotation.Difficulty;
+import dev.tuanm.sandbox.leetcode.annotation.Problem;
+import dev.tuanm.sandbox.leetcode.core.Solvable1;
+
+@Problem(
+        name = "Longest Substring Without Repeating Characters",
+        source = "https://leetcode.com/problems/longest-substring-without-repeating-characters/",
+        description = "Given a string s, find the length of the longest substring without repeating characters.",
+        difficulty = Difficulty.MEDIUM
+)
+public class LongestSubstringWithoutRepeatingCharacters implements Solvable1<String, Integer> {
+    @Override
+    public Integer solve(String s) {
+        if (s.length() < 2) return s.length();
+
+        Map<Character, Integer> presentCharsMap = new HashMap<>();
+        int max = 0;
+        int i = 0;
+        while (i < s.length()) {
+            presentCharsMap.clear();
+            int count = 0;
+            for (int j = i; j < s.length(); j++) {
+                char c = s.charAt(j);
+                Integer lastIndex = presentCharsMap.get(c);
+                if (lastIndex == null) {
+                    presentCharsMap.put(c, j);
+                    count++;
+                } else {
+                    i = lastIndex; // no need to find the longest substring from `lastIndex`
+                    break;
+                }
+            }
+            if (count > max) max = count;
+            i++; // re-find from the next character
+        }
+
+        return max;
+    }
+}
---
+++ src/test/java/dev/tuanm/sandbox/leetcode/problem/LongestSubstringWithoutRepeatingCharactersTest.java
@@ -0,0 +1,32 @@
+package dev.tuanm.sandbox.leetcode.problem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import dev.tuanm.sandbox.leetcode.ProblemTest;
+
+public class LongestSubstringWithoutRepeatingCharactersTest
+        extends ProblemTest<LongestSubstringWithoutRepeatingCharacters> {
+    @Override
+    protected LongestSubstringWithoutRepeatingCharacters problem() {
+        return new LongestSubstringWithoutRepeatingCharacters();
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    void testExamples(String s, int expected) {
+        assertEquals(expected, problem.solve(s));
+    }
+
+    static Stream<Arguments> source() {
+        return Stream.of(
+                Arguments.of("abcabcbb", 3),
+                Arguments.of("bbbbb", 1),
+                Arguments.of("pwwkew", 3));
+    }
+}
