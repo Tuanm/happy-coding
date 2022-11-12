04b38cf5eb1be385cb0ab94fb6131b4e4f00112d
Do Tuan <tuanm.dev@gmail.com>
2022-11-12 09:33:23 +0700

    [leetcode] add problem roman-to-integer

---
+++ src/test/java/dev/tuanm/sandbox/leetcode/problem/RomanToIntegerTest.java
@@ -0,0 +1,25 @@
+package dev.tuanm.sandbox.leetcode.problem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import dev.tuanm.sandbox.leetcode.ProblemTest;
+
+public class RomanToIntegerTest extends ProblemTest<RomanToInteger> {
+    @Override
+    protected RomanToInteger problem() {
+        return new RomanToInteger();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+        "III, 3",
+        "LVIII, 58",
+        "MCMXCIV, 1994"
+    })
+    void testExamples(String s, int expectation) {
+        assertEquals(expectation, problem.solve(s));
+    }
+}
