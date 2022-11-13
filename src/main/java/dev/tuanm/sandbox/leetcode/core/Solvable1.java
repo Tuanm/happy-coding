dbcc2208c944d7fa9b8191e5f2f97fafca48ba24
Do Tuan <tuanm.dev@gmail.com>
2022-11-13 16:26:41 +0700

    [leetcode] update Solvable-related interfaces inheritance

--- src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable1.java
+++ src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable1.java
@@ -2,7 +2,7 @@ package dev.tuanm.sandbox.leetcode.core;
 
 import dev.tuanm.sandbox.leetcode.exception.NotImplementedException;
 
-public interface Solvable1<I1, O> extends Solvable2<I1, Void, O> {
+public interface Solvable1<I1, O> extends Solvable<O> {
     default O solve(I1 i1) {
         throw new NotImplementedException();
     }
--- src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable2.java
+++ src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable2.java
@@ -2,7 +2,7 @@ package dev.tuanm.sandbox.leetcode.core;
 
 import dev.tuanm.sandbox.leetcode.exception.NotImplementedException;
 
-public interface Solvable2<I1, I2, O> extends Solvable3<I1, I2, Void, O> {
+public interface Solvable2<I1, I2, O> extends Solvable<O> {
     default O solve(I1 i1, I2 i2) {
         throw new NotImplementedException();
     }
