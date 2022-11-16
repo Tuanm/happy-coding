0726b455e2ed59cffe55b94675b5ebbaa84ecc93
Tuanm <tuandm1@vmodev.com>
2022-11-16 11:08:44 +0700

    [leetcode] add demo problem and set as default on App.java

--- src/main/java/dev/tuanm/sandbox/App.java
+++ src/main/java/dev/tuanm/sandbox/App.java
@@ -1,10 +1,28 @@
 package dev.tuanm.sandbox;
 
+import dev.tuanm.sandbox.leetcode.core.Solvable;
+import dev.tuanm.sandbox.leetcode.problem.demo.*;
+import dev.tuanm.sandbox.leetcode.util.Problems;
 import lombok.extern.java.Log;
 
 @Log
 public class App {
     public static void main(String[] args) {
-        log.info("Happy Coding!");
+        leetcode();
+    }
+
+    /**
+     * Tests a problem from
+     * <a href="https://leetcode.com/problems">LeetCode</a>.
+     */
+    static void leetcode() {
+        Solvable<Integer> problem = Problems.create(JustSum.class);
+
+        int a = 1073741823;
+        int b = 1344069815;
+        int expected = 2147483647;
+        boolean passed = problem.test(expected, a, b);
+
+        log.info(passed ? "PASSED" : "FAILED");
     }
 }
