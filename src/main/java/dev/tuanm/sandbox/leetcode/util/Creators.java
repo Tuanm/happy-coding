3d261dfeea5fde48a5a4d8756058792ffee1a2d9
Tuanm <gmail@tuanm.dev>
2022-11-25 21:35:28 +0700

    [leetcode] add more creators

--- src/main/java/dev/tuanm/sandbox/leetcode/util/Creators.java
+++ src/main/java/dev/tuanm/sandbox/leetcode/util/Creators.java
@@ -20,6 +20,55 @@ public final class Creators {
     }
 
     /**
+     * Returns a {@code double} array.
+     */
+    public static double[] doubles(double... vals) {
+        return vals;
+    }
+
+    /**
+     * Returns a {@code float} array.
+     */
+    public static float[] floats(float... vals) {
+        return vals;
+    }
+
+    /**
+     * Returns a {@code long} array.
+     */
+    public static long[] longs(long... vals) {
+        return vals;
+    }
+
+    /**
+     * Returns an {@code int} array.
+     */
+    public static int[] ints(int... vals) {
+        return vals;
+    }
+
+    /**
+     * Returns a {@code char} array.
+     */
+    public static char[] chars(char... vals) {
+        return vals;
+    }
+
+    /**
+     * Returns a {@code byte} array.
+     */
+    public static byte[] bytes(byte... vals) {
+        return vals;
+    }
+
+    /**
+     * Returns a {@code boolean} array.
+     */
+    public static boolean[] booleans(boolean... vals) {
+        return vals;
+    }
+
+    /**
      * Returns a list.
      *
      * @param <T> the type of the array's elements.
