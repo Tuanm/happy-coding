4ec0310614574524db990cccb634d2a6e9b8698c
Tuanm <tuandm1@vmodev.com>
2022-12-02 09:32:04 +0700

    [leetcode] optimize problem jump-game with dynamic programming approach

--- src/main/java/dev/tuanm/sandbox/leetcode/problem/JumpGame.java
+++ src/main/java/dev/tuanm/sandbox/leetcode/problem/JumpGame.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import dev.tuanm.sandbox.leetcode.annotation.Difficulty;
+import dev.tuanm.sandbox.leetcode.annotation.NotOptimizedYet;
 import dev.tuanm.sandbox.leetcode.annotation.Problem;
 import dev.tuanm.sandbox.leetcode.common.Tag;
 import dev.tuanm.sandbox.leetcode.core.Solvable1;
@@ -16,12 +17,29 @@ import dev.tuanm.sandbox.leetcode.core.Solvable1;
                 "You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.",
                 "Return true if you can reach the last index, or false otherwise."
         },
-        tag = Tag.GREEDY,
+        tag = { Tag.GREEDY, Tag.DYNAMIC_PROGRAMMING },
         difficulty = Difficulty.MEDIUM
 )
 public class JumpGame implements Solvable1<int[], Boolean> {
     @Override
     public Boolean solve(int[] nums) {
+        return this.dpSolve(nums);
+    }
+
+    protected boolean dpSolve(int[] nums) {
+        int[] max = new int[nums.length];
+
+        max[0] = 0;
+        for (int i = 1; i < nums.length; i++) {
+            max[i] = Math.max(max[i - 1], nums[i - 1]) - 1;
+            if (max[i] < 0) return false;
+        }
+
+        return max[nums.length - 1] >= 0;
+    }
+
+    @NotOptimizedYet
+    protected boolean greedySolve(int[] nums) {
         Set<Integer> jumpable = new HashSet<>();
 
         int index = nums.length - 1;
