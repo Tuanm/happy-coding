350dfb3fe183ff4980d0fad966da728f54e4b3e9
Tuan <47839860+Tuanm@users.noreply.github.com>
2023-01-27 17:19:53 +0700

    [leetcode] restructure interface Solveable (#3)

--- src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable1.java
+++ src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable1.java
@@ -1,15 +1,11 @@
 package dev.tuanm.sandbox.leetcode.core;
 
-import dev.tuanm.sandbox.leetcode.exception.NotSupportedException;
-
 public interface Solvable1<I1, O> extends Solvable<O> {
     O solve(I1 i1);
 
     @Override
     default O solve(Object... inputs) {
-        if (inputs.length < 1) {
-            throw new NotSupportedException();
-        }
+        this.validateInputs(inputs);
         I1 input1 = this.inputAt(0, inputs);
         return this.solve(input1);
     }
--- src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable4.java
+++ src/main/java/dev/tuanm/sandbox/leetcode/core/Solvable4.java
@@ -1,15 +1,11 @@
 package dev.tuanm.sandbox.leetcode.core;
 
-import dev.tuanm.sandbox.leetcode.exception.NotSupportedException;
-
 public interface Solvable4<I1, I2, I3, I4, O> extends Solvable<O> {
     O solve(I1 i1, I2 i2, I3 i3, I4 i4);
 
     @Override
     default O solve(Object... inputs) {
-        if (inputs.length < 4) {
-            throw new NotSupportedException();
-        }
+        this.validateInputs(inputs);
         I1 input1 = this.inputAt(0, inputs);
         I2 input2 = this.inputAt(1, inputs);
         I3 input3 = this.inputAt(2, inputs);
